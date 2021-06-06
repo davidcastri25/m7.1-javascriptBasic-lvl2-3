f4684ca69fb28f7e9509844ba69e7bf37aec2345
David Castrillón <david.castri@hotmail.es>
2021-06-06 18:03:01 +0200

    06/06/2021 - Inicio estructura de archivos y hago primera prueba

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+function init() {
+    //Variables
+    var display = document.getElementById("display");
+    var igual = document.getElementById("igual");
+    var clear = document.getElementById("clear");
+    var suma = document.getElementById("suma");
+    var resta = document.getElementById("resta");
+    var multiplicacion = document.getElementById("multiplicacion");
+    var division = document.getElementById("division");
+    var uno = document.getElementById("uno");
+    var dos = document.getElementById("dos");
+    var tres = document.getElementById("tres");
+    var cuatro = document.getElementById("cuatro");
+    var cinco = document.getElementById("cinco");
+    var seis = document.getElementById("seis");
+    var siete = document.getElementById("siete");
+    var ocho = document.getElementById("ocho");
+    var nueve = document.getElementById("nueve");
+    var cero = document.getElementById("cero");
+
+    //Eventos
+    
+}
