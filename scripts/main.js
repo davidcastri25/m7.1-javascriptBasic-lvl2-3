c3cde135a1c5d5bdd0177dc344f27d896eac061c
David Castrillón <david.castri@hotmail.es>
2021-06-07 12:03:06 +0200

    07/06/2021 - Retoco diseño y genero el código JS

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,114 @@
-function init() {
-    //Variables
-    var display = document.getElementById("display");
-    var igual = document.getElementById("igual");
-    var clear = document.getElementById("clear");
-    var suma = document.getElementById("suma");
-    var resta = document.getElementById("resta");
-    var multiplicacion = document.getElementById("multiplicacion");
-    var division = document.getElementById("division");
-    var uno = document.getElementById("uno");
-    var dos = document.getElementById("dos");
-    var tres = document.getElementById("tres");
-    var cuatro = document.getElementById("cuatro");
-    var cinco = document.getElementById("cinco");
-    var seis = document.getElementById("seis");
-    var siete = document.getElementById("siete");
-    var ocho = document.getElementById("ocho");
-    var nueve = document.getElementById("nueve");
-    var cero = document.getElementById("cero");
+//Variables
+var numero1 = "";
+var numero2 = "";
+var operacion;
+var operacionAnterior;
+var operacionMarcada = false;
+var primeraOperacion = true;
+var display = document.getElementById("display");
 
-    //Eventos
+function init() {
+    //Al iniciar, se mostrará un 0 en la pantalla
+    display.value = 0;    
+}
+
+
+//Función que mostrará por pantalla el número que se apreta
+function apretarNumero(num) {
+    if (operacionMarcada == false) {
+        numero1 += num;
+        display.value = numero1;
+    }
+    else {
+        numero2 += num;
+        display.value = numero2;
+    }
+
+}
+
+
+//Función llamada al apretar operador
+function operador(operador) {
+    let resultadoTemporal;
+
+    operacion = operador;
+    operacionMarcada = true; //Indicamos que hemos marcado una operación
+
+    if (primeraOperacion == false) { //Si no es la primera vez que marcamos, queremos que en la pantalla ya salga la operación anterior calculada
+        resultadoTemporal = calculadora(operacionAnterior, numero1, numero2);
+        numero1 = resultadoTemporal;
+
+        if (numero1 == "No se puede dividir entre 0, reseteando..."){
+            alert(numero1);
+            
+        }
+        else {
+            display.value = numero1;
+        }
+        
+        numero2 = "";
+    }
     
+    operacionAnterior = operacion; //La operación que nos calcula cuando le damos por segunda vez, es la de la primera
+
+    primeraOperacion = false;
+    //Si hemos dividido entre 0, reseteamos todo
+    if (numero1 == "No se puede dividir entre 0, reseteando...") {
+        limpiar();
+    }
+}
+
+
+function limpiar(){
+    numero1 = "";
+    numero2 = "";
+    operacion = "";
+    operacionAnterior = "";
+    operacionMarcada = false;
+    primeraOperacion = true;
+    display.value = 0;    
+}
+
+
+//Función de igual, para que calcule el resultado
+function igual(){
+    let resultado;
+    resultado = calculadora(operacion, numero1, numero2);
+    //Controlamos si hemos dividido entre 0
+    if (resultado == "No se puede dividir entre 0, reseteando...") {
+        alert("No se puede dividir entre 0, reseteando...");
+        limpiar();
+    }
+    else{
+        display.value = resultado;
+    }    
+}
+
+
+//Función que realiza el cálculo
+function calculadora(operacion, num1, num2) {
+    let resultado;
+    let numero1 = parseFloat(num1);
+    let numero2 = parseFloat(num2);
+
+    if (operacion == "/" && numero2 == 0){
+        resultado = "No se puede dividir entre 0, reseteando...";    
+    }
+    else {
+        switch (operacion) {
+        case "+":
+            resultado = numero1 + numero2;
+            break;
+        case "-":
+            resultado = numero1 - numero2;
+            break;
+        case "*":
+            resultado = numero1 * numero2;
+            break;
+        default:
+            resultado = numero1 / numero2;    
+        }    
+    }
+    
+    return resultado;
 }
